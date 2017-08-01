f835acf08e77a33029ac8cde93dab6d6a094fb65
xuhuanh <804284499@qq.com>
2017-08-01 21:36:44 +0800

    bom常用操作

---
+++ js/BOM/BOM.js
@@ -0,0 +1,24 @@
+/**
+ * Created by xuhuanhuan on 2017/8/1.
+ */
+
+var ua = navigator.userAgent;
+var isChrome = ua.indexOf('Chorme');
+console.log(ua);
+console.log(isChrome);
+
+//screen
+console.log(screen.width);
+console.log(screen.height);
+
+//location
+console.log(location.href);
+console.log(location.protocol);
+console.log(location.host);
+console.log(location.pathname);
+console.log(location.search);
+console.log(location.hash);
+
+//history
+history.back();
+history.forward();
